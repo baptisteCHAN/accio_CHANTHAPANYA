0a79100a39f78eba776cf578e51a540876e47946
Damien <Damien@Digdig>
2017-01-12 21:08:30 +0100

    commit nul pour merge

--- app_android/Test/app/src/main/java/com/example/damien/test/ReservationView.java
+++ app_android/Test/app/src/main/java/com/example/damien/test/ReservationView.java
@@ -5,6 +5,7 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ExpandableListView;
 import android.widget.Toast;
 import org.osmdroid.bonuspack.routing.GraphHopperRoadManager;
