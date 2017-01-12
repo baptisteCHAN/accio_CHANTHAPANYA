665a3a88bec5b7dbeef7c4ae1f908110b8b8c2fc
Chanthapanya Baptiste <baptiste.chanthapanya@ecole.ensicaen.fr>
2017-01-12 16:19:16 +0100

    modif interface graphique

--- app_android/Test/app/src/main/java/com/example/damien/test/loginActivity.java
+++ app_android/Test/app/src/main/java/com/example/damien/test/loginActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
